e20d22aeedb2174b32c6fbef6eaa49b62b963f14
prashant singh <prashantsingh061201@gmail.com>
2021-05-29 19:35:21 +0530

    first commit

--- src/MyComponents/Header.js
+++ src/MyComponents/Header.js
@@ -18,10 +18,10 @@ export default function Header(props) {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <Link className="nav-link active" aria-current="page" to="/">Home</Link>
+                <Link className="nav-link active fs-5" aria-current="page" to="/">Home</Link>
               </li>
               <li className="nav-item">
-                <Link className="nav-link" to="/about">About</Link>
+                <Link className="nav-link fs-5" to="/about">About</Link>
               </li>  
             </ul>
             { props.searchBar? <form className="d-flex">
